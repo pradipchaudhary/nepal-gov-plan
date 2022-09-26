fbf77c0e7331898fc8eef61f7b56b50debd91573
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-09-26 15:38:08 +0545

    create server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
-console.log(app);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Hello "));
+
+// console.log(app);
